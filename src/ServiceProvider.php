1a0f154a6486d394080e80a119a7ab5a3bd3c2fa
Felix <felix@srv1523168.hstgr.cloud>
2026-06-04 18:57:34 +0300

    Initial release

---
+++ src/ServiceProvider.php
@@ -0,0 +1,7 @@
+<?php
+namespace FelixMuhoro;
+use Illuminate\Support\ServiceProvider as Base;
+class ServiceProvider extends Base {
+    public function boot(): void {}
+    public function register(): void {}
+}
